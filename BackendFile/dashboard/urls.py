2abc97a48ff7a383a4779b02d156e3de60a77206
Arooba047 <aroobaiqbal118@gmail.com>
2024-10-22 14:07:30 +0500

    Integrating API to display menu in frontend

--- BackendFile/dashboard/urls.py
+++ BackendFile/dashboard/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
 from .import views
+from .views import MenuItemDisplay
+
+
+
+
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
@@ -9,5 +14,6 @@ urlpatterns = [
     path('delete/<int:id>/', views.delete, name='delete'),
     path('charts/', views.charts, name='charts'),
     path('widgets/', views.widgets, name='widgets'),
+    path('menudisplay/', MenuItemDisplay.as_view(), name='MenuDisplay'),
 ]
 
--- BackendFile/dashboard/views.py
+++ BackendFile/dashboard/views.py
@@ -6,6 +6,11 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import MenuItemForm
+from .serializers import MenuItemSerializer
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import JsonResponse
 
 @login_required
 def dashboard(request):
@@ -18,13 +23,6 @@ def table(request):
         'user':user
     }
     return render(request,"table.html",context)
-# @login_required
-# def forms(request):
-#     # menu = MenuItem.objects.order_by('-id')
-#     # context = {
-#     #     'menu' : menu
-#     # }
-#     return render(request,"form.html")
 
 
 # Create and Read Menu
@@ -79,3 +77,18 @@ def charts(request):
 @login_required
 def widgets(request):
     return render(request,"widget.html")
+
+
+# Display Menu Data in React
+
+class MenuItemDisplay(APIView):
+    def get(self, request):
+        menuitem = MenuItem.objects.all()
+        menuserializer = MenuItemSerializer(menuitem, many=True)
+        return JsonResponse(menuserializer.data, safe=False)
+
+
+
+
+
+
